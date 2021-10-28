c3b51da05e711edf3f215b4d79bd2675b29e1bba
MUDDASIR HASSAN <M.HASSAN-2021@hull.ac.uk>
2021-10-28 17:23:45 +0100

    First Commit

---
+++ hello.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace FirstApp_Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
